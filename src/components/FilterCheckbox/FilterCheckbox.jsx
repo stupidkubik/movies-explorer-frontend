fc7d8a10dac7e64136e19ca043b09f1bf821306d
Evgenii Rubin <kubikus2009@yandex.ru>
2023-11-07 16:20:38 +0300

    feat: стилизация чекбокса

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,12 +1,14 @@
 import { React } from 'react';
+// Стили для свитча
 // https://www.w3schools.com/howto/howto_css_switch.asp
-// https://codepen.io/AllThingsSmitty/pen/MmxxOz/
 
 function FilterCheckbox() {
   return (
     <div className="search__checkbox">
-      <input type="checkbox" className="checkbox" />
-      <span className="checkbox__custom" />
+      <label htmlFor="checkbox" className="checkbox__container">
+        <input id="checkbox" type="checkbox" className="checkbox" />
+        <span className="checkbox__custom" />
+      </label>
       <h2 className="checkbox__text">Короткометражки</h2>
     </div>
   );
